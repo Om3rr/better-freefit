c1d8ce233b7cfe3f25224484b616d192a5795d2a
Omer <omer.shacham@ridewithvia.com>
2021-12-20 20:38:33 +0200

    progress

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import BetterMap from './components/BetterMap';
 import * as clubs from './data/clubs.json'
--- webapp/src/components/BetterMap.jsx
+++ webapp/src/components/BetterMap.jsx
@@ -1,34 +1,31 @@
-import React, {useState} from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React from "react";
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 import './BetterMap.css'
-import { Icon } from "leaflet";
-// import * as parkData from "./data/skateboard-parks.json";
-function parseLatLng(club) {
-  const [lat, lng, _] = club.lat_lng.replace("@", "").split(",")
-  return [parseFloat(lat), parseFloat(lng)]
-}
+
 
 function BetterMap({clubs, selectedTags}) {
-  console.log(clubs)
-  return (
-    clubs && <MapContainer center={[32.09, 34.77]} zoom={15}scrollWheelZoom={true}>
-      <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    {clubs.filter(club => Array.isArray(club.lat_lng)).map((club) => (
-      <Marker key={club.id} position={club.lat_lng} title={club.title}>
-        <Popup>
-          <div className="popup-container">
-          <h3>{club.title}</h3>
-          {club.about}
-          <div className="tags">{club.tags.map((t, idx) => <span className={`tag ${selectedTags.indexOf(t) > -1 ? "selected" : ""}`} key={idx}>{t}</span>)}</div>
-          </div>
-        </Popup>
-      </Marker>
-    ))}
-    </MapContainer>
-  );
+    console.log(clubs)
+    return (
+        clubs && <MapContainer center={[32.09, 34.77]} zoom={15} scrollWheelZoom={true}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {clubs.filter(club => Array.isArray(club.lat_lng)).map((club) => (
+                <Marker key={club.id} position={club.lat_lng} title={club.title}>
+                    <Popup>
+                        <div className="popup-container">
+                            <h3>{club.title}</h3>
+                            {club.about}
+                            <div className="tags">{club.tags.map((t, idx) => <span
+                                className={`tag ${selectedTags.indexOf(t) > -1 ? "selected" : ""}`}
+                                key={idx}>{t}</span>)}</div>
+                        </div>
+                    </Popup>
+                </Marker>
+            ))}
+        </MapContainer>
+    );
 }
 
 export default BetterMap;
